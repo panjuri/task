20ef2d3423b5534602b1439ab84e37a445052e37
Puneet Anujuri <panjuri@umail.iu.edu>
2017-05-24 13:30:44 -0400

    everthing before Day 7

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import Header from './Header'
+import ThingList from './ThingList'
 
 class App extends Component {
+  state = {
+    things: {
+      'thing-1': { id: 'thing-1', name: 'Milk' },
+      'thing-2': { id: 'thing-2', name: 'Bread' },
+      'thing-3': { id: 'thing-3', name: 'Bibb lettuce' },
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <ThingList things={this.state.things} />
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+        <header>
+      <h1>ThingList</h1>
+      <h2>So Many Things</h2>
+    </header>
+  )
+}
+}
+
+export default Header
